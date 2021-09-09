ce07dc1d17421fd2d2263ab96a648005238ee35b
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2021-09-09 10:36:41 +0000

    Import aarch64-elf: Gnu/Gcc-11.2.1 + Gnu/Binutils 2.37 (20210909)
    aarch64-elf-gcc:
    
    Target: aarch64-elf
    Supported LTO compression algorithms: zlib zstd
    gcc version 11.2.1 20210909 (MoonBase-Android-ToolChain)
    
    https://github.com/gcc-mirror/gcc/commit/f459220912d46d6fe3d05fac711b8bf06dd6d98d
    
    aarch64-elf-ld.bfd:
    
    GNU ld (jsX-Custom Gnu/Binutils) 2.37.20210909
    
    https://github.com/bminor/binutils-gdb/commit/52a3d18e3ef50ea74959271175118f61087edb88
    
    NOTE:
    Only C Language Include
    Main Purpose Kernel Cross Compilation
    
    Date: Thursday, September 09 2021
    
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- lib/gcc/aarch64-elf/11.1.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/root/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion=MoonBase-Android-ToolChain --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
---
+++ lib/gcc/aarch64-elf/11.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --prefix=/home/jsx/gnu/gcc_moon/../gcc-arm64 --enable-languages=c --enable-obsolete --enable-secureplt --disable-werror --with-build-config=bootstrap-lto --with-system-zlib --without-included-gettext --disable-decimal-float --disable-esp --enable-poison-system-directories --disable-libatomic --enable-threads --with-long-double-128 --disable-bootstrap --enable-__cxa_atexit --enable-clocale=gnu --enable-multilib --disable-fixed-point --disable-libssp --disable-libada --disable-systemtap --disable-valgrind-annotations --disable-vtable-verify --disable-libvtv --with-zstd --enable-lto --with-isl --disable-isl-version-check --disable-libsanitizer --disable-default-pie --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --disable-default-ssp --with-headers --enable-static --with-pkgversion=MoonBase-Android-ToolChain --with-gnu-as --with-gnu-ld --with-newlib --with-sysroot";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
